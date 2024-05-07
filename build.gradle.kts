19465c50629d703adaac0e2ad74f41f8217b56b0
Oleg Yukhnevich <whyoleg@gmail.com>
2024-05-07 21:41:12 +0300

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
     kotlin("multiplatform") version "2.0.0-RC2"
 }
 
-val linuxOpensslPath = layout.projectDirectory.dir("openssl/linux-x64/lib").asFile.absolutePath
-
 kotlin {
     @OptIn(ExperimentalKotlinGradlePluginApi::class)
     compilerOptions {
@@ -22,19 +20,14 @@ kotlin {
             else               -> error("Unknown os.arch: $javaOsArch")
         }
 
-        javaOsName.contains("linux", ignoreCase = true)   -> linuxX64("native") {
-            binaries.configureEach {
-                linkerOpts("-L$linuxOpensslPath")
-            }
-        }
-
+        javaOsName.contains("linux", ignoreCase = true)   -> linuxX64("native")
         javaOsName.contains("windows", ignoreCase = true) -> mingwX64("native")
         else                                              -> error("Unknown os.name: $javaOsName")
     }
 
     sourceSets {
         nativeMain.dependencies {
-            implementation(cryptographyLibs.provider.openssl3.shared)
+            implementation(cryptographyLibs.provider.openssl3.prebuilt)
         }
 
         nativeTest.dependencies {
